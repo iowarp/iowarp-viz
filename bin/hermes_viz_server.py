61c55403ac31ac5a1a49a41f1d27ada0ad11a690
Luke Logan <lukemartinlogan@gmail.com>
2025-03-06 23:12:20 -0600

    Add mock viz

--- bin/hermes_viz_server.py
+++ bin/hermes_viz_server.py
@@ -1,5 +1,22 @@
 from mock_mdm import MetadataSnapshot
+from flask import Flask, jsonify
 
 mdm = MetadataSnapshot()
 mdm.collect()
 
+app = Flask(__name__)
+
+@app.route('/api/tags', methods=['GET'])
+def get_tags():
+    return jsonify(mdm.tags)
+
+@app.route('/api/targets', methods=['GET'])
+def get_targets():
+    return jsonify(mdm.targets)
+
+@app.route('/api/blobs', methods=['GET'])
+def get_blobs():
+    return jsonify(mdm.blobs)
+
+if __name__ == '__main__':
+    app.run(debug=True)

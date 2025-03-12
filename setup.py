8e0bf428dd3c6eabce0c5b83713c019a1aee0520
lukemartinlogan <lukemartinlogan@gmail.com>
2025-03-12 10:59:20 -0500

    Return the iowarp html as a flask server

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import setuptools
+import os
+import shutil
 
 setuptools.setup(
     name="py_hermes_mdm",
     packages=setuptools.find_packages(),
+    scripts=['bin/hermes_viz_server'],
     version="0.0.1",
     author="Luke Logan",
     author_email="lukemartinlogan@gmail.com",
@@ -26,3 +29,11 @@ setuptools.setup(
         "flask"
     ]
 )
+
+home = os.path.expanduser("~")
+dashboard_src = os.path.join(os.path.dirname(__file__), "dashboard")
+dashboard_dst = os.path.join(home, ".hermes_viz")
+
+if os.path.exists(dashboard_dst):
+    shutil.rmtree(dashboard_dst)
+shutil.copytree(dashboard_src, dashboard_dst)
